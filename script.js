7d158da170b919d19f1895492876d3a5ea0ac13b
devjanaprime <dev@primeacademy.io>
2016-10-20 14:08:24 -0500

    added addSugar and displaySugar functions

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 console.log( 'test' );
 // I can't make compressed zip files...
+var allTheSugar = 0;
+
+var addSugar = function( amount ){
+  console.log( 'in addSugar:', amount );
+  allTheSugar += amount;
+  console.log( 'sugar:', allTheSugar );
+  displaySugar();
+}; // end addSugar
+
+var displaySugar = function(){
+  console.log( 'in displaySugar');
+  var outputLabel = document.getElementById( 'sugarOut' ).innerHTML = allTheSugar;
+}; // end displaySugar
