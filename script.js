d9d55d7a7d6387b7c671d37400838a55b19d21be
devjanaprime <dev@primeacademy.io>
2016-10-20 14:12:31 -0500

    added verbose option for console logs

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 console.log( 'test' );
 // I can't make compressed zip files...
 var allTheSugar = 0;
+var verbose = false;
 
 var addSugar = function( amount ){
-  console.log( 'in addSugar:', amount );
+  if( verbose ) console.log( 'in addSugar:', amount );
   allTheSugar += amount;
-  console.log( 'sugar:', allTheSugar );
+  if( verbose ) console.log( 'sugar:', allTheSugar );
   displaySugar();
 }; // end addSugar
 
 var displaySugar = function(){
-  console.log( 'in displaySugar');
-  var outputLabel = document.getElementById( 'sugarOut' ).innerHTML = allTheSugar;
+  if( verbose ) console.log( 'in displaySugar');
+  document.getElementById( 'sugarOut' ).innerHTML = allTheSugar;
 }; // end displaySugar
